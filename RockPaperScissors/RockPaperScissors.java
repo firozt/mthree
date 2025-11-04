cca4e4d9c208f77ad0761c50fb70ce7380cc5094
firozt <firozt03@gmail.com>
2025-11-04 21:32:56 +0000

    finished rock paper scissors

--- RockPaperScissors/RockPaperScissors.java
+++ RockPaperScissors/RockPaperScissors.java
@@ -2,74 +2,96 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
+//    global variable for all the options
     private final static String[] choices = {"Rock","Paper","Scissors"};
+
     public static void main(String[] args) {
         boolean playAgain = true;
 
         while (playAgain) {
             runGame();
 
-            String playAgainInput = askInputStr("Do you want to play again? (No/Yes)").toLowerCase();
-            playAgain = playAgainInput.equals("yes") ? true : false;
+            String playAgainInput = getInputStr("Do you want to play again? (No/Yes)").toLowerCase();
+            playAgain = playAgainInput.equals("yes");
         }
         System.exit(0);
-
     }
 
     public static void runGame() {
         // round input and validation
-        int numberOfRounds = askInputInt("How many rounds would you like to play? (1-10)");
+        int numberOfRounds = getInputInt("How many rounds would you like to play? (1-10)");
 
         if (numberOfRounds < 0 || numberOfRounds > 10) {
             System.exit(1);
         }
 
         // Game variables
-        int cpu_wins = 0;
-        int player_wins = 0;
+        int CPUWins = 0;
+        int playerWins = 0;
         int ties = 0;
         Random random = new Random();
 
         // Game loop
         for(int round = 0; round < numberOfRounds; round++) {
-            System.out.println(String.format("Round %d of %d",round+1, numberOfRounds));
-            int player_choice = askInputInt("Please pick one of the following\n(1) Rock \n(2) Paper \n(3) Scissors");
-            int cpu_choice = random.nextInt(3)+1; // range of 1 -> 3 (both inclusive)
+            System.out.printf("Round %d of %d%n",round+1, numberOfRounds);
 
-            System.out.println(String.format("CPU Chose %s", choices[cpu_choice-1]));
+            int playerChoice = getUserChoice();
+            int CPUChoice = random.nextInt(3)+1; // range of 1 -> 3 (both inclusive)
+
+            System.out.printf("CPU Chose %s%n", choices[CPUChoice-1]);
             // tied
-            if (player_choice == cpu_choice) {
+            if (playerChoice == CPUChoice) {
                 System.out.println("Tied!");
                 ties++;
             }
             // player wins
-            else if (
-                (player_choice == 1 && cpu_choice == 3) ||
-                (player_choice == 2 && cpu_choice == 1) ||
-                (player_choice == 3 && cpu_choice == 2)
-            ) {
+            else if (isPlayerWinner(playerChoice,CPUChoice)) {
                 System.out.println("Player Wins!\n");
-                player_wins++;
+                playerWins++;
             }
             // player loses
             else {
                 System.out.println("CPU Wins!\n");
-                cpu_wins++;
+                CPUWins++;
             }
         }
 
         // Show summary of rounds
-        String winner = player_wins == cpu_wins ? "Tied" : player_wins > cpu_wins ? "Player Wins" : "CPU Wins"; 
-        System.out.println(String.format("Ties: %d\nPlayer Wins: %d\nCPU Wins: %d \nWinner is %s!",ties,player_wins,cpu_wins,winner));
-
+        String winner = playerWins == CPUWins ? "Tied" : playerWins > CPUWins ? "Player Wins" : "CPU Wins";
+        System.out.printf("Ties: %d\nPlayer Wins: %d\nCPU Wins: %d \nWinner is %s!%n",ties, playerWins, CPUWins,winner);
     }
 
-    public static int askInputInt(String message) {
+//    Checks to see if player won
+    public static boolean isPlayerWinner(int playerChoice, int cpuChoice) {
+        return (playerChoice == 1 && cpuChoice == 3) ||     // rock beats scissors
+                (playerChoice == 2 && cpuChoice == 1) ||    // paper beats rock
+                (playerChoice == 3 && cpuChoice == 2);      // scissors beats paper
+    }
+
+    public static int getUserChoice() {
+        int userChoice = -1;
+
+        do {
+            System.out.println("Please pick one of the following:");
+            for (int i = 0; i < choices.length; i++) {
+                System.out.println("(" + (i + 1) + ") " + choices[i]);
+            }
+            userChoice = getInputInt("Your choice: ");
+            if (userChoice < 1 || userChoice > choices.length) {
+                System.out.println("Invalid choice. Please enter a number between 1 and " + choices.length + ".");
+            }
+
+        } while (userChoice < 0 || userChoice >= choices.length);
+
+        return userChoice;
+    }
+
+    public static int getInputInt(String message) {
         System.out.println(message);
         Scanner input = new Scanner(System.in);
         return input.nextInt();
     }
-    public static String askInputStr(String message) {
+    public static String getInputStr(String message) {
         System.out.println(message);
         Scanner input = new Scanner(System.in);
         return input.nextLine();
